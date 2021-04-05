831f09c8b7b658c4a1da4ae1ffbca3a65b91d7d7
GENESTE Alexandre <alex.geneste3@gmail.com>
2021-04-05 11:51:55 +0200

    finish

--- src/question1/Cook.java
+++ src/question1/Cook.java
@@ -2,4 +2,17 @@ package question1;
 
 public class Cook {
 
+    private Strategy strategy;
+
+    public Strategy getStrategy() {
+        return strategy;
+    }
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public RiceCookingWays cookRice() {
+        return this.strategy.cookRice();
+    }
 }
--- src/question2/Cooking.java
+++ src/question2/Cooking.java
@@ -1,5 +1,20 @@
 package question2;
 
 public class Cooking {
+    private RiceType riceType;
 
+    Cooking(RiceType riceType) {
+        this.riceType = riceType;
+    }
+
+    public String prepareDinner() {
+        String sentence = "I am done cooking ";
+        if (this.riceType == RiceType.BASMATI) {
+            return sentence + RiceType.BASMATI.toString();
+        } else if (this.riceType == RiceType.JAPONICA) {
+            return sentence + RiceType.JAPONICA.toString();
+        } else {
+            return sentence + RiceType.CAMARGUE.toString();
+        }
+    }
 }
